0200fa20f921823947850aec111128073a302dd1
raghvendra1218 <raghvendra.dixit@sjsu.edu>
2019-06-30 00:42:01 -0700

    Add Groups and Post views, and html pages

--- socialmediasite/groups/views.py
+++ socialmediasite/groups/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import (LoginRequiredMixin,
+										 PermissionRequiredMixin)
+from django.urls import reverse
+from django.views.generic import CreateView, DeleteView, DetailView, ListView
+from groups.models import Group, GroupMember
 
 # Create your views here.
+class CreateGroup(LoginRequiredMixin, CreateView):
+	fields = ('name', 'description')
+	model = Group
+
+
+class SingleGroup(DeleteView):
+	model = Group
+
+
+class ListGroups(ListView):
+	model = Group
