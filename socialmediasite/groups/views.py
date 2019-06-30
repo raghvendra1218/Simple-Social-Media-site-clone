b209bff43c87b03fc5e9091c7562c46d30742e08
raghvendra1218 <raghvendra.dixit@sjsu.edu>
2019-06-30 01:22:45 -0700

    Add Join and Leave groups functionality in models

--- socialmediasite/groups/views.py
+++ socialmediasite/groups/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from django.contrib import messages
 from django.contrib.auth.mixins import (LoginRequiredMixin,
 										 PermissionRequiredMixin)
 from django.urls import reverse
-from django.views.generic import CreateView, DeleteView, DetailView, ListView
+from django.views.generic import (CreateView, DeleteView, 
+								  DetailView, ListView, 
+								  RedirectView)
 from groups.models import Group, GroupMember
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 class CreateGroup(LoginRequiredMixin, CreateView):
@@ -17,3 +21,44 @@ class SingleGroup(DeleteView):
 
 class ListGroups(ListView):
 	model = Group
+
+
+class JoinGroup(LoginRequiredMixin, RedirectView):
+
+	def get_redirect_url(self, *args, **kwargs):
+		return reverse('groups:single',kwargs={'slug':self.kwargs.get('slug')})
+
+
+	def get(self, request,*args,**kwargs):
+		group = get_object_or_404(Group, slug = self.kwargs.get('slug'))
+
+		try:
+			GroupMember.objects.create(user=self.request.user, group=group)
+		except:
+			messages.warning(self.request,'Warning, already a member!')
+		else:
+			messages.success(self.request,'You are now a member!')
+
+		return super().get(request,*args,**kwargs)
+
+
+class LeaveGroup(LoginRequiredMixin, RedirectView):
+
+	def get_redirect_url(self, *args, **kwargs):
+		return reverse('groups:single',kwargs={'slug':self.kwargs.get('slug')})
+
+
+	def get(self, request,*args,**kwargs):
+
+		try:
+			membership = GroupMember.objects.filter(
+				user=self.request.user,
+				group__slug= self.kwargs.get('slug')
+			).get()
+		except GroupMember.DoesNotExists:
+			messages.warning(self.request,'Sorry you are not in this group!')
+		else:
+			membership.delete()
+			messages.success(self.request,'You have left the group!')
+
+		return super().get(request,*args,**kwargs)	
